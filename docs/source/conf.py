c4805b05a874490e2d9b36717f526f2c19101122
dvp <d.portnov@iterrf.ru>
2023-01-23 18:35:44 +0300

    build: poetry update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -172,4 +172,4 @@ latex_elements: Dict[str, str] = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, document class [howto, manual, or own class]).
-latex_documents = [(master_doc, "mapstp.tex", "mapstp Documentation", "Dmitry Portnov", "manual")]
+latex_documents = [(master_doc, "xpypact.tex", "xpypact Documentation", "Dmitry Portnov", "manual")]
