936f8f356232dc8a8bfc1ee4121b47fcd887e088
dvp <d.portnov@iterrf.ru>
2025-07-10 21:32:20 +0300

    dependencies: stop support python 3.10

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,36 +1,31 @@
+"""Test if installed package is of the current version."""
+
 from __future__ import annotations
 
 import re
-import sys
+import tomllib
 
 from pathlib import Path
 from re import sub as substitute
 
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import tomli as tomllib
-
 from xpypact import __version__
 
 
-def find_version_from_project_toml():
+def _find_version_from_project_toml() -> str:
     toml_path = Path(__file__).parent.parent / "pyproject.toml"
     assert toml_path.exists()
     pyproject = tomllib.loads(toml_path.read_text())
-    return pyproject["tool"]["poetry"]["version"]
+    return pyproject["project"]["version"]
 
 
 _VERSION_NORM_PATTERN = re.compile(r"-(?P<letter>.)[^.]*\.(?P<prepatch>.*)$")
 
 
-def normalize_version(version: str):
+def _normalize_version(version: str) -> str:
     return substitute(_VERSION_NORM_PATTERN, r"\1\2", version)
 
 
-def test_package():
+def test_package() -> None:
     """This test checks if only current version is installed in working environment."""
-    version = find_version_from_project_toml()
-    assert __version__ == normalize_version(
-        version,
-    ), "Run 'poetry install' and, if this doesn't help, run `tools/clear-prev-dist-info.py`"
+    version = _find_version_from_project_toml()
+    assert __version__ == _normalize_version(version), "Run 'uv install'"
